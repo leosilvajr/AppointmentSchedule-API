e5351aba4d257bd86ac76ff461f238fc353ef72a
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2024-12-29 22:03:18 -0300

    Iiniciando API em DOTNET

---
+++ AppointmentSchedule-API/Controllers/DoctorController.cs
@@ -0,0 +1,24 @@
+﻿using AppointmentSchedule.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AppointmentSchedule_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DoctorController : ControllerBase
+    {
+        [HttpGet]
+        public List<Doctor> Index()
+        {
+            var lista = new List<Doctor>();
+            lista.Add(new Doctor(1 , "João", "Cardiologista", "M"));
+            lista.Add(new Doctor(2, "Maria", "Dermatologista", "F"));
+            lista.Add(new Doctor(3, "Pedro", "Ortopedista", "M"));
+            lista.Add(new Doctor(4, "Ana", "Oftalmologista", "F"));
+            lista.Add(new Doctor(5, "Lucas", "Ginecologista", "M"));
+
+            return lista;
+        }
+    }
+}
---
+++ AppointmentSchedule.Model/Doctor.cs
@@ -0,0 +1,23 @@
+﻿namespace AppointmentSchedule.Model
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public string Icon { get; set; }
+
+        public Doctor()
+        {
+            
+        }
+        public Doctor(int id, string name, string specialty, string icon)
+        {
+            Id = id;
+            Name = name;
+            Specialty = specialty;
+            Icon = icon;
+        }
+
+    }
+}
