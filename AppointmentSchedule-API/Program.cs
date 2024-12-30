1c92582b098eab185843ccc395c34ab515c67530
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2024-12-30 15:00:43 -0300

    Novo Projeto com Migration EF Core Funcionando

--- AppointmentSchedule-API/Program.cs
+++ AppointmentSchedule-API/Program.cs
@@ -1,15 +1,11 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using AppointmentSchedule.Infra.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
 
+builder.Services.AddControllers();
 
 // Register ApplicationDbContext with SQL Server connection
 builder.Services.AddDbContext<AppointmentScheduleContext>(option =>
@@ -18,15 +14,9 @@ builder.Services.AddDbContext<AppointmentScheduleContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("AppointmentSchedule-API"));
 });
 
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
 
 app.UseHttpsRedirection();
 
