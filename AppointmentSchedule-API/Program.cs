dadcd8b22266979e2d195d78f0a7cc73778b8767
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-01-01 18:40:22 -0300

    Implementado Documentação Swagger

--- AppointmentSchedule-API/Program.cs
+++ AppointmentSchedule-API/Program.cs
@@ -4,6 +4,8 @@ using AppointmentSchedule.Repository.Repository;
 using AppointmentSchedule.Service.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using AppointmentSchedule.Service.Services;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,7 @@ builder.Services.AddScoped<IDoctorService, DoctorService>();
 
 builder.Services.AddControllers();
 
+
 // Register ApplicationDbContext with SQL Server connection
 builder.Services.AddDbContext<AppointmentScheduleContext>(option =>
 {
@@ -21,9 +24,38 @@ builder.Services.AddDbContext<AppointmentScheduleContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("AppointmentSchedule-API"));
 });
 
+
+// Configuração do Swagger
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "AppointmentSchedule-API",
+            Version = "v1",
+            Description = "A user-friendly app designed to streamline medical appointment scheduling and management efficiently.",
+            Contact = new OpenApiContact
+            {
+                Name = "Leonardo Silva",
+                Url = new Uri("https://github.com/leosilvajr")
+            }
+        });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Configuração do Swagger
+app.UseSwagger();
+app.UseSwaggerUI(view => {
+    view.SwaggerEndpoint("/swagger/v1/swagger.json", "API - Authentication");
+});
+
+// Redireciona a rota raiz para o Swagger
+var option = new RewriteOptions();
+option.AddRedirect("^$", "swagger");
+app.UseRewriter(option);
 
 app.UseHttpsRedirection();
 
